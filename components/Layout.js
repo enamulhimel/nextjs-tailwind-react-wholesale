0ea695634d2673eec16cd83d1b97855afd14cea5
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2023-06-01 01:12:37 +0600

    cookie setup

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { Store } from '@/utilities/Store';
@@ -6,6 +6,10 @@ import { Store } from '@/utilities/Store';
 export default function Layout({ title, children }) {
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
+  const [cartItemsCount, SetCartItemsCount] = useState(0);
+  useEffect(() => {
+    SetCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
   return (
     <>
       <Head>
@@ -22,9 +26,9 @@ export default function Layout({ title, children }) {
             <div>
               <Link href="/cart" className="p-2">
                 Cart
-                {cart.cartItems.length > 0 && (
+                {cartItemsCount > 0 && (
                   <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
-                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    {cartItemsCount}
                   </span>
                 )}
               </Link>
