86fb3ed1d5b8b45618b379b7479c47b804bda89c
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2023-06-01 22:49:34 +0600

    seed simple product

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,10 +1,13 @@
 import db from '@/utilities/db';
 import User from '@/models/User';
 import data from '@/utilities/data';
+import Product from '@/models/Product';
 const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ massage: 'seeded succesfully' });
 };
